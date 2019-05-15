8e74fcd915e75b06b058b5c5b072a36ed3b8a7c8
yuhe3000 <yanliqiang66@gmail.com>
2019-05-15 10:52:09 +1000

    add webpack react babel

---
+++ src/components/Todolist.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todolist = () => ( 
+  <div>
+    hello
+  </div>
+)
+
+export default Todolist;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todolist from './components/Todolist';
+
+ReactDOM.render(<Todolist />, document.querySelector('#app'));
